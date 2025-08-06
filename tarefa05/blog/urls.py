d163b917ba2016a1c9a3b047fe5bbe0456f9a17d
arturfew <arturfelipesouza64@gmail.com>
2025-08-06 14:31:00 -0300

    tarefa 05 sem as postagens

---
+++ tarefa05/blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name='index'),
+    path("noticia/<int:id_post>", views.noticia, name='noticia')
+    ]
