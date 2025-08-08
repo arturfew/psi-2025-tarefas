dc4868c29e884d8ce7f12b571e8a834040b06cdb
arturfew <arturfelipesouza64@gmail.com>
2025-08-08 16:28:42 -0300

    tarefa 05 concluida.

--- tarefa05/blog/models.py
+++ tarefa05/blog/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 from datetime import date
-# Create your models here.
+
 class Postagem(models.Model):
     titulo = models.CharField( max_length=50)
     descricao = models.TextField()
     data = models.DateField(date.today)
     imagem = models.ImageField( upload_to="media/",null=True, blank=True)
-    def __str__(self):
-        return self.titulo
+    
 class Header(models.Model):
     titulo = models.CharField(max_length=50)
     link = models.CharField(max_length=100)
+
     def __str__(self):
         return self.titulo
+    
 class Footer(models.Model):
     descricao = models.TextField()
-    
+    def __str__(self):
+
+        return self.titulo
+
